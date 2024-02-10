8ca3f4e077b96f43098e52f64229073c01907af3
OscarEpp <crazytrain303@gmail.com>
2024-02-09 17:30:35 -0800

    Initial Commit

---
+++ LandmarksV3/LandmarksV3App.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksV3App.swift
+//  LandmarksV3
+//
+//  Created by Oscar Epp on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksV3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
